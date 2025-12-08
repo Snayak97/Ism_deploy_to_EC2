6ebcc28c79baf23f5c8218b90c7a9da6d5e41fd8
Soumya <soumyanayak963@gmail.com>
2025-12-08 23:30:12 +0530

    bug fixrsb

--- fontend/src/pages/masterupload/UploadMasterData.jsx
+++ fontend/src/pages/masterupload/UploadMasterData.jsx
@@ -13,7 +13,8 @@ import {
   ListItemText,
 } from "@mui/material";
 import AllDataTables from "./AllDataTables";
-import MasterAllData from "../Masteralldata/MasterAllData";
+// import MasterAllData from "../Masteralldata/MasterAllData";
+import MasterAllData from "./../masteralldata/MasterAllData.jsx";
 
 const UploadMasterData = () => {
   const fileInputRef = useRef(null);
